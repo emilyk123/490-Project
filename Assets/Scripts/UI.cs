bad21d89b4f8c7b519f7b782b1cb2eccb9086159
Emily Krohn <emilykrohn@csu.fullerton.edu>
2025-05-15 13:20:19 -0700

    Recreate Shop UI

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,9 +5,49 @@ public class UI : MonoBehaviour
 {
     [SerializeField] UIDocument uiDoc;
 
-    void Start()
+    private Button _healthBuyButton;
+    private Button _keyBuyButton;
+    private Button _upgradeBuyButton;
+    private Button _cancelButton;
+    VisualElement displayUI;
+
+    private void Start()
     {
-        VisualElement root = uiDoc.rootVisualElement;
-        root.style.display = DisplayStyle.None;
+        displayUI = uiDoc.rootVisualElement;
+
+        displayUI.RegisterCallback<PointerDownEvent>(evt => Debug.Log("PointerDown at UI root"));
+
+        _healthBuyButton = uiDoc.rootVisualElement.Q("healthBuy") as Button;
+        _keyBuyButton = uiDoc.rootVisualElement.Q("keyBuy") as Button;
+        _upgradeBuyButton = uiDoc.rootVisualElement.Q("upgradeBuy") as Button;
+        _cancelButton = uiDoc.rootVisualElement.Q("cancel") as Button;
+
+        _healthBuyButton?.RegisterCallback<ClickEvent>(OnHealthBuyClicked);
+        _keyBuyButton?.RegisterCallback<ClickEvent>(OnKeyBuyClicked);
+        _upgradeBuyButton?.RegisterCallback<ClickEvent>(OnUpgradeBuyClicked);
+        _cancelButton?.RegisterCallback<ClickEvent>(OnCancelClicked);
+        
+        displayUI.style.display = DisplayStyle.None;
+    }
+
+    private void OnDisable()
+    {
+        _healthBuyButton?.UnregisterCallback<ClickEvent>(OnHealthBuyClicked);
+        _keyBuyButton?.UnregisterCallback<ClickEvent>(OnKeyBuyClicked);
+        _upgradeBuyButton?.UnregisterCallback<ClickEvent>(OnUpgradeBuyClicked);
+        _cancelButton?.UnregisterCallback<ClickEvent>(OnCancelClicked);
+    }
+
+    private void OnHealthBuyClicked(ClickEvent evt) {
+        Debug.Log("Buy Health");
+    }
+    private void OnKeyBuyClicked(ClickEvent evt) {
+        Debug.Log("Buy Key");
+    }
+    private void OnUpgradeBuyClicked(ClickEvent evt) {
+        Debug.Log("Buy Upgrade");
+    }
+    private void OnCancelClicked(ClickEvent evt) {
+        displayUI.style.display = DisplayStyle.None;
     }
 }
